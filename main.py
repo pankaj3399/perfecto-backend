0419219059eddbe59e24deaaad63c63e952e51ea
pankaj3399 <vatwanipankaj74@gmail.com>
2024-06-07 11:28:08 +0530

    Allow CORS

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from crud import get_random_properties
 from models import Property
 from typing import List
@@ -7,6 +8,15 @@ import uvicorn
 import os
 
 app = FastAPI()
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/recommendedProperties", response_model=List[Property])
 async def recommended_properties():
